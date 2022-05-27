fd36e76e86d1b0a9500a423e513b309888412bcb
HoangChung <hoangchungk53qx1@gmail.com>
2022-05-27 13:21:58 +0700

    widget perm

--- lib/pages/home/components/carousel_items.dart
+++ lib/pages/home/components/carousel_items.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../../../models/carousel_item_model.dart';
@@ -9,101 +8,99 @@ import '../../../utils/constants.dart';
 List<CarouselItemModel> carouselItems = List.generate(
   5,
   (index) => CarouselItemModel(
-    text: Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            "Mobile Developer",
-            style: GoogleFonts.oswald(
-              color: kPrimaryColor,
-              fontWeight: FontWeight.w900,
-              fontSize: 16.0,
-            ),
+    text: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          "Mobile Developer",
+          style: GoogleFonts.oswald(
+            color: kPrimaryColor,
+            fontWeight: FontWeight.w900,
+            fontSize: 16.0,
           ),
-          const SizedBox(
-            height: 18.0,
+        ),
+        const SizedBox(
+          height: 18.0,
+        ),
+        Text(
+          "HOANG ANH CHUNG",
+          style: GoogleFonts.oswald(
+            color: Colors.white,
+            fontSize: 40.0,
+            fontWeight: FontWeight.w900,
+            height: 1.3,
           ),
-          Text(
-            "HOANG ANH CHUNG",
-            style: GoogleFonts.oswald(
-              color: Colors.white,
-              fontSize: 40.0,
-              fontWeight: FontWeight.w900,
-              height: 1.3,
-            ),
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        const Text(
+          "Full-stack mobile developer, based in HaNoi",
+          style: TextStyle(
+            color: kCaptionColor,
+            fontSize: 15.0,
+            height: 1.0,
           ),
-          const SizedBox(
-            height: 10.0,
-          ),
-          const Text(
-            "Full-stack mobile developer, based in HaNoi",
-            style: TextStyle(
-              color: kCaptionColor,
-              fontSize: 15.0,
-              height: 1.0,
-            ),
-          ),
-          const SizedBox(
-            height: 10.0,
-          ),
-          Wrap(
-            children: [
-              const Text(
-                "Need a full custom mobile?",
-                style: TextStyle(
-                  color: kCaptionColor,
-                  fontSize: 15.0,
-                  height: 1.5,
-                ),
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        Wrap(
+          children: [
+            const Text(
+              "Need a full custom mobile?",
+              style: TextStyle(
+                color: kCaptionColor,
+                fontSize: 15.0,
+                height: 1.5,
               ),
-              GestureDetector(
-                onTap: () {},
-                child: const MouseRegion(
-                  cursor: SystemMouseCursors.click,
-                  child: Text(
-                    " Got a project? Let's talk.",
-                    style: TextStyle(
-                      height: 1.5,
-                      color: Colors.white,
-                      fontSize: 15.0,
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 25.0,
-          ),
-          MouseRegion(
-            cursor: SystemMouseCursors.click,
-            child: Container(
-              decoration: BoxDecoration(
-                color: kPrimaryColor,
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              height: 48.0,
-              padding: const EdgeInsets.symmetric(
-                horizontal: 28.0,
-              ),
-              child: TextButton(
-                onPressed: () {},
-                child: const Text(
-                  "GET STARTED",
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: const MouseRegion(
+                cursor: SystemMouseCursors.click,
+                child: Text(
+                  " Got a project? Let's talk.",
                   style: TextStyle(
+                    height: 1.5,
                     color: Colors.white,
-                    fontSize: 13.0,
-                    fontWeight: FontWeight.bold,
+                    fontSize: 15.0,
                   ),
                 ),
               ),
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 25.0,
+        ),
+        MouseRegion(
+          cursor: SystemMouseCursors.click,
+          child: Container(
+            decoration: BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.circular(8.0),
             ),
-          )
-        ],
-      ),
+            height: 48.0,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 28.0,
+            ),
+            child: TextButton(
+              onPressed: () {},
+              child: const Text(
+                "GET STARTED",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 13.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
     ),
     image: Image.asset(
       "assets/person.png",
